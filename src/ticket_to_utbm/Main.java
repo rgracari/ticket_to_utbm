b81088d0e3f83f4e992a187f8b76167613d852ed
rgrbot <rgrbotaddr@gmail.com>
2020-12-18 16:59:39 +0100

    testing collaborator

--- src/ticket_to_utbm/Main.java
+++ src/ticket_to_utbm/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("TICKET TO UTBM!");
+		System.out.println("ADD A COLABORATOR");
 	}
 
 }
